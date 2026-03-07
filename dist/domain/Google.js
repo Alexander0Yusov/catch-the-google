cfbf700227868e5555bfdd454415d8d1071005bc
Alexander0Yusov <yusovsky2@gmail.com>
2026-03-07 18:50:56 +0200

    chore: remove dist from repository and apply gitignore

--- dist/domain/Google.js
+++
@@ -1,6 +0,0 @@
-import { Unit } from "./Unit.js";
-export class Google extends Unit {
-    constructor(position) {
-        super(position);
-    }
-}
--- dist/domain/NumberUtil.js
+++
@@ -1,5 +0,0 @@
-export class NumberUtil {
-    static getRandomNumber(max) {
-        return Math.floor(Math.random() * max + 1);
-    }
-}
